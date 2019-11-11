2bb55a6b5093b7813adc4c86178df8d48f123a84
YWJ <ywj.hy@aliyun.com>
2019-11-12 00:56:54 +0800

    去除部分页面的vant并重写布局

--- components/popup/popup.js
+++ components/popup/popup.js
@@ -4,14 +4,33 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    'customStyle': {
+      type: String,
+      value: ''
+    },
+    'overlayStyle': {
+      type: String,
+      value: ''
+    },
+    'height': {
+      type: String,
+      value: '30%'
+    },
+    'show': {
+      type: Boolean,
+      value: false
+    },
+    'overlay': {
+      type: Boolean,
+      value: true
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    // Height: ''
   },
 
   /**
@@ -19,5 +38,11 @@ Component({
    */
   methods: {
 
-  }
+  },
+  // attached () {
+  //   let Height = this.properties.height
+  //   this.setData({
+  //     Height
+  //   })
+  // }
 })
