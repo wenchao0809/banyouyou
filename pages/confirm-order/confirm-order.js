48158a3c868816a42400faff66299e69ffc46f2d
ET <estding0@gmail.com>
2019-11-13 17:25:33 +0800

    确认订单

---
+++ pages/confirm-order/confirm-order.js
@@ -0,0 +1,18 @@
+Page({
+    data: {
+        goodList: [{
+            title: '5厘米全杨油漆板',
+            price: 4499.00,
+            total: 10,
+            count: 1,
+            image: '/image/quick3.jpg'
+          }, 
+          {
+            title: '阻燃中纤板',
+            price: 124,
+            count: 10,
+            size: '15mm',
+            image: '/image/quick3.jpg'
+          }]
+    }
+})
